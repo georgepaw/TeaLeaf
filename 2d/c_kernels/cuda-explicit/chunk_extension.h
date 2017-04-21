7206ff7c2ea75125ae8e252f8f84a34f95da60a3
George Pawelczak <gp13823.2013@my.bristol.ac.uk>
2017-04-21 07:19:34 -0500

    Start of explicit (CSR) CG method for CUDA

---
+++ 2d/c_kernels/cuda-explicit/chunk_extension.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef __CHUNKEXTENSIONH
+#define __CHUNKEXTENSIONH
+
+typedef double* FieldBufferType;
+
+// Empty extension point
+typedef struct ChunkExtension
+{
+    double*   d_comm_buffer;
+    double*   d_reduce_buffer;
+    double*   d_reduce_buffer2;
+    double*   d_reduce_buffer3;
+    double*   d_reduce_buffer4;
+    uint32_t* d_row_index;
+    uint32_t* d_col_index;
+    double*   d_non_zeros;
+} ChunkExtension;
+
+#endif
