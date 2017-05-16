352cfd045a85ff53f38fb64c357c462aa74bac01
Grzegorz Pawelczak <gp13823@my.bristol.ac.uk>
2017-05-16 18:03:35 +0100

    Add Hamming and Hsiao codes for SECDED

--- 2d/ABFT/CPU/ecc_double_vector.h
+++ 2d/ABFT/CPU/ecc_double_vector.h
@@ -43,6 +43,7 @@ static inline double check_ecc_double(double * in, uint32_t * flag)
   uint64_t secded_in = all_bits & 0xFFULL;
 
   uint64_t bits = all_bits & 0xFFFFFFFFFFFFFF00ULL;
+#if defined(HSIAO)
   uint32_t syndrome =
         __builtin_parityll((S1 & bits) ^ (secded_in & C1))
       | __builtin_parityll((S2 & bits) ^ (secded_in & C2)) << 1
@@ -55,7 +56,6 @@ static inline double check_ecc_double(double * in, uint32_t * flag)
 
   if(parity)
   {
-  printf("0x%" PRIX64 "\n", all_bits);
     if(syndrome)
     {
       int32_t bit_position = get_fliped_bit_location(syndrome);
@@ -80,6 +80,50 @@ static inline double check_ecc_double(double * in, uint32_t * flag)
       (*flag)++;
     }
   }
+#elif defined(HAMMING)
+  uint32_t syndrome =
+        __builtin_parityll((S1 & bits) ^ (secded_in & C1))
+      | __builtin_parityll((S2 & bits) ^ (secded_in & C2)) << 1
+      | __builtin_parityll((S3 & bits) ^ (secded_in & C3)) << 2
+      | __builtin_parityll((S4 & bits) ^ (secded_in & C4)) << 3
+      | __builtin_parityll((S5 & bits) ^ (secded_in & C5)) << 4
+      | __builtin_parityll((S6 & bits) ^ (secded_in & C6)) << 5
+      | __builtin_parityll((S7 & bits) ^ (secded_in & C7)) << 6;
+
+  if(parity)
+  {
+    if(syndrome)
+    {
+      int32_t bit_position = get_fliped_bit_location(syndrome);
+      if(bit_position < 0)
+      {
+        printf("Uncorrectable error with odd number of bitflips\n");
+        (*flag)++;
+      }
+      else
+      {
+        all_bits ^= 0x1ULL << bit_position;
+        *in = *((double*)&all_bits);
+        printf("Correctable error found bit %d\n", bit_position);
+      }
+    }
+    else
+    {
+      all_bits ^= 0x1ULL << 7; //fix parity
+      *in = *((double*)&all_bits);
+      printf("Correctable error found - parity bit\n");
+    }
+  }
+  else
+  {
+    if(syndrome)
+    {
+      printf("Uncorrectable error with even number of bitflips\n");
+      (*flag)++;
+    }
+  }
+#endif
+
 #endif
   return *((double*)&all_bits);
 }
@@ -93,6 +137,7 @@ static inline double add_ecc_double(double in)
 #elif defined(ABFT_METHOD_DOUBLE_VECTOR_SECDED)
   all_bits &= 0xFFFFFFFFFFFFFF00ULL;
 
+#if defined(HSIAO)
   const int secded_bits[] =
   {
     __builtin_parityll(S1 & all_bits),
@@ -105,13 +150,34 @@ static inline double add_ecc_double(double in)
     __builtin_parityll(S8 & all_bits)
   };
   all_bits |= (secded_bits[0]
-          | secded_bits[1] << 1
-          | secded_bits[2] << 2
-          | secded_bits[3] << 3
-          | secded_bits[4] << 4
-          | secded_bits[5] << 5
-          | secded_bits[6] << 6
-          | secded_bits[7] << 7);
+           | secded_bits[1] << 1
+           | secded_bits[2] << 2
+           | secded_bits[3] << 3
+           | secded_bits[4] << 4
+           | secded_bits[5] << 5
+           | secded_bits[6] << 6
+           | secded_bits[7] << 7);
+
+#elif defined(HAMMING)
+  const int secded_bits[] =
+  {
+    __builtin_parityll(S1 & all_bits),
+    __builtin_parityll(S2 & all_bits),
+    __builtin_parityll(S3 & all_bits),
+    __builtin_parityll(S4 & all_bits),
+    __builtin_parityll(S5 & all_bits),
+    __builtin_parityll(S6 & all_bits),
+    __builtin_parityll(S7 & all_bits)
+  };
+  all_bits |= (secded_bits[0]
+           | secded_bits[1] << 1
+           | secded_bits[2] << 2
+           | secded_bits[3] << 3
+           | secded_bits[4] << 4
+           | secded_bits[5] << 5
+           | secded_bits[6] << 6);
+  all_bits |= __builtin_parityll(all_bits) << 7;
+#endif
 
 #endif
   return *((double*)&all_bits);
